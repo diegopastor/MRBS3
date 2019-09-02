9c47627f94378634b574ca9aa157194a4e58ea42
diego <dpcalvo@gmail.com>
2019-09-01 22:57:19 -0500

    Add fill base and get query functions

--- src/generator.js
+++ src/generator.js
@@ -1,5 +1,18 @@
 'use strict';
 
+require('prototypes');
+
+const KEYWORD_PREFIX = '$';
+const KEYWORD_DIVIDER = '-';
+const TABLES = {
+  sp: ['special', 'specials'],
+  ob: ['object', 'objects'],
+  adj: ['adjective', 'adjectives'],
+  gen: ['gen_per', 'gender'],
+  ver: ['verb', 'verbs'],
+  per: ['person', 'people'],
+};
+
 exports.generateTweet = callback => {
   getBase((error, base) => {
     if (error) return callback(error, null);
@@ -10,11 +23,37 @@ exports.generateTweet = callback => {
   });
 };
 
-// Gets a Random Base
-// Out -> A String that is an unfilled base
-function getBase(callback) {}
+function getBase(callback) {
+  let i = Math.floor(Math.random() * Math.floor(bases.length));
+  return callback(null, bases[i]);
+}
 
-// Fills a Base
-// In -> A String that is an unfilled base
-// Out -> A String that is a filled base
-function fillBase(base, callback) {}
+function fillBase(base, callback) {
+  while (base.contains(KEYWORD_PREFIX)) {
+    let leftmostToReplace = base
+      .slice(base.indexOf(KEYWORD_PREFIX) + 1)
+      .substringUpTo(' ')
+      .substringUpTo('.')
+      .substringUpTo(',')
+      .substringUpTo('"');
+    let table = leftmostToReplace.split(KEYWORD_DIVIDER)[0];
+    let category = leftmostToReplace.split(KEYWORD_DIVIDER)[1];
+    buildQuery(table, category, query => {
+      getReplacement(query, (error, replacement) => {
+        if (error) return callback(error);
+        base = base.replace(KEYWORD_PREFIX + leftmostToReplace, replacement);
+      });
+    });
+  }
+  return callback(null, base);
+}
+
+function buildQuery(table, category, callback) {
+  singularOfTable = TABLES[table][0];
+  table = TABLES[table][1];
+  return callback(
+    `SELECT ${singularOfTable} FROM ${table} WHERE TAG = ${category} ORDER BY RAND() LIMIT 1`,
+  );
+}
+
+function getReplacement(query, callback) {}
